7dbf84c45ce24368f64d72b4d9524e2474378457
Geok0de <gns25107@naver.com>
2017-12-06 21:34:41 +0900

    로그인 화면, 로그인 REST API 구현 (인증 문제 수정 필요)

--- UML01Board/src/main/java/com/koitt/board/controller/UserRestController.java
+++ UML01Board/src/main/java/com/koitt/board/controller/UserRestController.java
@@ -1,5 +1,7 @@
 package com.koitt.board.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,8 @@ import com.koitt.board.service.UserInfoService;
 
 public class UserRestController {
 
+	private Logger logger = LogManager.getLogger(this.getClass());
+
 	@Autowired
 	private UserInfoService userInfoService;
 
@@ -22,6 +26,9 @@ public class UserRestController {
 	// /user/login POST REST API 추가하기...
 	@RequestMapping(value = "/user/login", method = RequestMethod.POST)
 	public ResponseEntity<String> login(UserInfo userInfo) {
+
+		logger.debug(userInfo);
+
 		// 아이디 존재 유무와 비밀번호 일치 여부 확인
 		boolean isMatched = userInfoService.isPasswordMatched(userInfo.getEmail(), userInfo.getPassword());
 
@@ -33,9 +40,12 @@ public class UserRestController {
 			// 평문을 Base64로 인코딩
 			String base64Credentials = new String(Base64.encodeBase64(plainCredentials.getBytes()));
 
+			logger.debug(base64Credentials);
+
 			return new ResponseEntity<String>(base64Credentials, HttpStatus.OK);
 		}
 
+		logger.debug("login failed");
 		return new ResponseEntity<String>("", HttpStatus.NOT_FOUND);
 	}
 
